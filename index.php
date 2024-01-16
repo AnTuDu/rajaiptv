7bfdd1d56d983b11744c8dc545347ae73b54d8be
AnTuDu <109011425+AnTuDu@users.noreply.github.com>
2024-01-16 00:33:25 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Redirect Page</title>
-</head>
-<body>
 <?php
-  // Get the 'user' parameter from the URL
-  $userParam = isset($_GET['user']) ? $_GET['user'] : null;
+$userParam = isset($_GET['user']) ? $_GET['user'] : null;
 
-  // Check if the 'user' parameter is present
-  if ($userParam) {
-    // Construct the redirection URL
+if ($userParam) {
     $redirectTo = 'https://tvj.rediptv.live/a?user=' . urlencode($userParam);
-
-    // Perform the redirect
     header("Location: $redirectTo", true, 302);
     exit;
-  } else {
-    // Display a message for the original request
+} else {
     echo '<h1>Original request</h1>';
-  }
+}
 ?>
-</body>
-</html>
