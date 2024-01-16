e286438945755bfca76f0bdfffaae6484796a120
AnTuDu <109011425+AnTuDu@users.noreply.github.com>
2024-01-16 00:31:31 -0800

    Update index.php

--- index.php
+++ index.php
@@ -4,28 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Redirect Page</title>
-  <script>
-    document.addEventListener('DOMContentLoaded', function () {
-      // Get the 'user' parameter from the URL
-      const urlParams = new URLSearchParams(window.location.search);
-      const userParam = urlParams.get('user');
-
-      // Check if the 'user' parameter is present
-      if (userParam) {
-        // Construct the redirection URL
-        const redirectTo = `https://tvj.rediptv.live/a?user=${encodeURIComponent(userParam)}`;
-
-        // Replace the current history entry without triggering navigation
-        window.history.replaceState({}, document.title, '/');
-
-        // Redirect the user
-        window.location.replace(redirectTo);
-      } else {
-        // Display a message for the original request
-        document.body.innerHTML = '<h1>Original request</h1>';
-      }
-    });
-  </script>
 </head>
 <body>
 <?php
